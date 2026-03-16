f8a5187687d765553ddc1fe0d7db2ee76fb5a669
AlexMjbl <a.belanger132@gmail.com>
2026-03-15 20:54:11 -0400

    added redirection to index page when refrehsing with empty data

--- src/pages/dailyPage.tsx
+++ src/pages/dailyPage.tsx
@@ -3,6 +3,7 @@ import type { City } from "../types/city";
 import type { DailyWeather } from "../types/dailyWeather";
 import DailyList from "../components/daily/dailyList";
 import DailyTempGraph from "../components/daily/dailyTempGraph";
+import { Navigate } from "react-router-dom";
 
 type Props = {
     dailyWeather?: DailyWeather
@@ -11,7 +12,7 @@ type Props = {
 
 
 export default function DailyPage({ dailyWeather, city }: Props) {
-    if (!dailyWeather) return null
+       if (!dailyWeather) return <Navigate to="/" replace />
     return (
         <div className="max-w-5xl mx-auto p-4 space-y-6">
 
--- src/pages/hourlyPage.tsx
+++ src/pages/hourlyPage.tsx
@@ -2,6 +2,7 @@ import HourlyList from "../components/hourly/hourlyList";
 import HourlyTempGraph from "../components/hourly/hourlyTempGraph";
 import type { City } from "../types/city";
 import type { HourlyWeather } from "../types/hourlyWeather";
+import { Navigate } from "react-router-dom";
 
 type Props = { 
     hourlyWeather?: HourlyWeather
@@ -9,7 +10,7 @@ type Props = {
 }
 
 export default function HourlyPage({ hourlyWeather, city }: Props) {
-     if (!hourlyWeather) return null
+     if (!hourlyWeather) return <Navigate to="/" replace />
     
     return (
         <div className="max-w-5xl mx-auto p-4 space-y-6">
